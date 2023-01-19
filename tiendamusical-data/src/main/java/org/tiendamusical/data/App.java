8373bc70c7ba5611ced43458306f618c921797e3
Uriel Castañeda Sierra <114254837+urielcastaneda@users.noreply.github.com>
2023-01-19 17:12:17 -0500

    version inicial del proyecto final de tienda musical

---
+++ tiendamusical-data/src/main/java/org/tiendamusical/data/App.java
@@ -0,0 +1,13 @@
+package org.tiendamusical.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
